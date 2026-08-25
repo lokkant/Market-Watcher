5d04d646d74c32ab12e8619affc256ff2eddd769
Daniel <83646403+lokkant@users.noreply.github.com>
2026-08-25 11:36:08 +0300

    update Alert.scala

--- src/main/scala/domain/Alert.scala
+++ src/main/scala/domain/Alert.scala
@@ -4,8 +4,31 @@ import java.util.UUID
 
 opaque type AlertId = UUID
 
+object AlertId {
+  def apply(): AlertId = UUID.randomUUID()
+  extension (alertId: AlertId) def value: UUID = alertId
+}
+
 enum Direction {
   case Below, Above
 }
 
-case class Alert(id: AlertId, symbol: Symbol, threshold: BigDecimal, direction: Direction, isActive: Boolean)
+case class Alert(
+    id: AlertId,
+    symbol: Symbol,
+    baseCurrency: Symbol,
+    threshold: BigDecimal,
+    direction: Direction,
+    isActive: Boolean
+)
+
+object Alert {
+  def apply(
+      symbol: Symbol,
+      baseCurrency: Symbol,
+      threshold: BigDecimal,
+      direction: Direction,
+      isActive: Boolean = true
+  ): Alert = 
+    new Alert(AlertId(), symbol, baseCurrency, threshold, direction, isActive)
+}
